cdfa97eb38f2345aa1a13b40af664f1d5d7dd5be
lory105 <giacomo.lory@gmail.com>
2012-07-04 14:52:26 +0200

    primo

---
+++ src/it/lory/fragment/MainActivity.java
@@ -0,0 +1,18 @@
+package it.lory.fragment;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class MainActivity extends Activity {
+	
+/** Called when the activity is first created. */
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
